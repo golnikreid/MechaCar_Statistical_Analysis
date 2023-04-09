62ff486909a5e80471c86525519e229a9be8c969
golnikreid <golnikreid@gmail.com>
2023-04-09 13:48:32 -0400

    Add Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -26,4 +26,37 @@ names(lot_summary)[names(lot_summary) == 'median(PSI)'] <- 'Median'
 names(lot_summary)[names(lot_summary) == 'var(PSI)'] <- 'Variance'
 names(lot_summary)[names(lot_summary) == 'sd(PSI)'] <- 'SD'
 
+# Deliverable 3
+
+alaska <- subset(sardines, location == 1)
+mean(alaska$vertebrae)
+# Calculate the population mean for Sardine Vertebrae in San Diego.
+# Hint: use the subset() function to get only the data for San Diego.
+sandiego <- subset(sardines, location == 6)
+mean(sandiego$vertebrae)
+# Calculate Independent (Two Sample) T-Test
+t.test(alaska$vertebrae, sandiego$vertebrae)
+
+t.test(coil$PSI, mu = 1500)
+
+lot1 <- subset(coil, Manufacturing_Lot == "Lot1")
+lot2 <- subset(coil, Manufacturing_Lot == "Lot2")
+lot3 <- subset(coil, Manufacturing_Lot == "Lot3")
+
+t.test(lot1$PSI, mu = 1500)
+t.test(lot2$PSI, mu = 1500)
+t.test(lot3$PSI, mu = 1500)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
